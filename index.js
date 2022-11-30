be7dd65ab58382176ad55c37affc4b77c7ae5dac
SayemAbdullah1 <dxsayem0@gmail.com>
2022-11-30 15:17:39 +0600

    add deleting code

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
+
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-
+const { query } = require('express');
 const port = process.env.PORT || 5000;
 require('dotenv').config()
 
-const app = express()
+const app = express();
+
 //middleware
 app.use(cors())
 app.use(express.json())
@@ -16,6 +18,27 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1q1hbsc.mongodb.net/?retryWrites=true&w=majority`;
 // console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+// function verifyJWT(req, res, next) {
+
+//     const authHeader = req.headers.authorization;
+//     if (!authHeader) {
+//         return res.status(401).send('unauthorized access');
+//     }
+
+//     const token = authHeader.split(' ')[1];
+
+//     jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+//         if (err) {
+//             return res.status(403).send({ message: 'forbidden access' })
+//         }
+//         req.decoded = decoded;
+//         next();
+//     })
+
+// }
+
 
 
 async function run(){
@@ -47,6 +70,12 @@ async function run(){
 
         app.get('/bookings', async (req, res) => {
             const email = req.query.email;
+            //jwt token
+            // const decodedEmail = req.decoded.email;
+            // if (email !== decodedEmail) {
+            //     res.status(403).send({ message: 'forbiden access' })
+            // }
+            console.log(email);
             const query = { email: email }
             const bookings = await bookingsCollection.find(query).toArray()
             res.send(bookings)
@@ -83,16 +112,31 @@ async function run(){
             const user = await usersCollection.find(query).toArray()
             res.send(user)
         })
+
+        app.get('/users/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email }
+            const user = await usersCollection.findOne(query);
+            res.send({ isAdmin: user?.role === 'admin' });
+        })
+
+
         //json web token connection
-        app.get('/jwt', async (req, res) => {
-            const email = req.query.email;
-            const query = { email: email }
-            const user = await usersCollection.findOne(query)
-            if (user) {
-                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '2h' })
-                return res.send({ accessToken: token })
-            }
-            res.status(403).send({ accessToken: '' })
+        // app.get('/jwt', async (req, res) => {
+        //     const email = req.query.email;
+        //     const query = { email: email };
+        //     const user = await usersCollection.findOne(query);
+        //     if (user) {
+        //         const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+        //         return res.send({ accessToken: token });
+        //     }
+        //     res.status(403).send({ accessToken: '' })
+        // });
+        app.delete('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) }
+            const result = await usersCollection.deleteOne(filter)
+            res.send(result)
         })
     }
     finally{
